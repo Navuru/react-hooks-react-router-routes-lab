b4abbc989f366abddc5105e8d0e580a9d498026f
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-09-29 08:56:48 +0300

    Final

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,7 +7,7 @@ const movieList = movies.map((movie) => (
   <div 
   key={movie.title}>
     <h2>{movie.title}</h2>
-    <h3>{movie.time}</h3>
+    <h3>Watch time :{movie.time}</h3>
     <ul>
       {movie.genres.map((genre) => (
         <li key ={genre}>
